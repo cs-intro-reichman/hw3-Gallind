1d8aa79bd93ebdfbe0e9897b991de3299793fb28
Gallind <gal.lind@post.runi.ac.il>
2024-11-18 20:08:54 +0200

    test1

--- Algebra.java
+++ Algebra.java
@@ -6,7 +6,7 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
+	   /* System.out.println(plus(2,3));   // 2 + 3
 	    System.out.println(minus(7,2));  // 7 - 2
    		System.out.println(minus(2,7));  // 2 - 7
  		System.out.println(times(3,4));  // 3 * 4
@@ -17,7 +17,8 @@ public class Algebra {
    		System.out.println(div(5,5));    // 5 / 5  
    		System.out.println(div(25,7));   // 25 / 7
    		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6    
+   		System.out.println(mod(120,6));  // 120 % 6   
+		*/ 
    		System.out.println(sqrt(36));
 		System.out.println(sqrt(263169));
    		System.out.println(sqrt(76123));
@@ -26,42 +27,80 @@ public class Algebra {
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
 		// Replace the following statement with your code
-		return 0;
+		for (int i = 0; i < x2; i++){
+			x1++;
+		}
+		return x1;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
 		// Replace the following statement with your code
-		return 0;
+		for (int i = 0; i < x2; i++){
+			x1--;
+		}
+		return x1;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
         // Replace the following statement with your code
-		return 0;
+		int firstX1 = x1;
+		for (int i = 1; i < x2; i++){
+
+			x1 = plus(x1, firstX1);
+		}
+		return x1;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
 		// Replace the following statement with your code
-		return 0;
+		int solution = 1;
+		for (int i = 0; i < n; i++){
+			solution = times(solution, x);
+		}
+		return solution;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
         // Replace the following statement with your code
-		return 0;
+		int solution = 0;
+		while (x1 >= x2){
+			solution++;
+			x1 = minus(x1, x2);
+		}
+		return solution;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
         // Replace the following statement with your code
-		return 0;
+		int solution;
+		solution = minus(x1, times(x2, div(x1, x2)));
+		return solution;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
         // Replace the following statement with your code
-		return 0;
+		int i = 0;
+
+        while (times((plus(i, 1)), (plus(i, 1))) <= x) {
+            i++;
+        }
+
+        // Compare i and i + 1 to find the closer one
+        int diff1 = minus(x, times(i, i));             // Difference with i^2
+        int diff2 = minus(times(plus(i, 1), plus(i, 1)), x); // Difference with (i + 1)^2
+
+        if (diff1 <= diff2) {
+            return i;
+        } else {
+            return i++;
+        }
+
+		
 	}	  	  
 }
