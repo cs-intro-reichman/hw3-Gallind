9391eca6d42271c3107c5a20cfed46f33bc47e37
Gallind <gal.lind@post.runi.ac.il>
2024-11-20 19:04:54 +0200

    check

--- Algebra.java
+++ Algebra.java
@@ -6,7 +6,7 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	   /* System.out.println(plus(2,3));   // 2 + 3
+	    System.out.println(plus(2,3));   // 2 + 3
 	    System.out.println(minus(7,2));  // 7 - 2
    		System.out.println(minus(2,7));  // 2 - 7
  		System.out.println(times(3,4));  // 3 * 4
@@ -18,15 +18,37 @@ public class Algebra {
    		System.out.println(div(25,7));   // 25 / 7
    		System.out.println(mod(25,7));   // 25 % 7
    		System.out.println(mod(120,6));  // 120 % 6   
-		*/ 
    		System.out.println(sqrt(36));
 		System.out.println(sqrt(263169));
    		System.out.println(sqrt(76123));
+		System.out.println(plus(-2,-5));
+		System.out.println(abs(-5));
+		System.out.println(abs(5));
+		//System.out.println(times(-1,-7));
+		System.out.println(div(-7,-2));
 	}  
 
+
+	public static int abs(int x){
+		int newX = x;
+		if (x < 0){
+			int i = 0;
+			
+			while (i > x * 2){
+				newX++;
+				i--;
+				
+				//System.out.println(x);
+			}
+		}
+		return newX;
+	}
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
 		// Replace the following statement with your code
+		if (x2 < 0){
+			return minus(x1, abs(x2));
+		}
 		for (int i = 0; i < x2; i++){
 			x1++;
 		}
@@ -36,6 +58,9 @@ public class Algebra {
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
 		// Replace the following statement with your code
+		if (x2 < 0){
+			return plus(x1, abs(x2));
+		}
 		for (int i = 0; i < x2; i++){
 			x1--;
 		}
@@ -46,10 +71,20 @@ public class Algebra {
 	public static int times(int x1, int x2) {
 		// Replace the following statement with your code
 		int firstX1 = x1;
-		for (int i = 1; i < x2; i++){
-
-			x1 = plus(x1, firstX1);
+		if (x1 == 0 || x2 == 0){
+			return 0;
 		}
+		if (x2 > 0){
+			for (int i = 1; i < x2; i++){
+				x1 = plus(x1, firstX1);
+				}
+		} 
+		else {
+			for (int i = 1; i > x2; i--){
+				x1 = minus(x1, firstX1);
+			}
+		}	
+		
 		return x1;
 	}
 
@@ -67,10 +102,46 @@ public class Algebra {
 	public static int div(int x1, int x2) {
 		// Replace the following statement with your code
 		int solution = 0;
-		while (x1 >= x2){
-			solution++;
-			x1 = minus(x1, x2);
+		if (x1 >= 0 && x2 > 0){
+			while (x1 >= x2){
+				solution++;
+				x1 = minus(x1, x2);
+			}
+			return solution;
 		}
+		if ((x1 < 0 && x2 > 0) || (x1 > 0 && x2 < 0)){
+			while (abs(x1) >= abs(x2)){
+				solution--;
+				x1 = minus(abs(x1), abs(x2));
+			}
+			return solution;
+		}
+		if (x1 < 0 && x2 < 0){
+			x1 = abs(x1);
+			x2 = abs(x2);
+			while (x1 >= x2){
+				solution++;
+				x1 = minus(x1, x2);
+			}
+			return solution;
+		}
+		/*if ((x1 < 0 && x2 > 0) || (x1 > 0 && x2 < 0)){
+			while (x1 >= x2){
+				solution--;
+				x1 = minus(abs(x1), abs(x2));
+			}
+			return solution;
+		}
+			
+		if (x1 < 0 && x2 < 0){
+			while (x1 >= x2){
+				solution++;
+				x1 = minus(abs(x1), abs(x2));
+			}
+			return solution;
+		}
+			*/
+
 		return solution;
 	}
 
