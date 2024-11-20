fe0aad24d4c03d347a358d2c78b8ea9936255012
Gallind <gal.lind@post.runi.ac.il>
2024-11-20 17:36:33 +0200

    c

--- Anagram.java
+++ Anagram.java
@@ -59,13 +59,39 @@ public class Anagram {
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
 		// Replace the following statement with your code
-		return "";
+		str = str.toLowerCase();
+		char[] letters = {'a','b','c','d','e','f','g','h','i','j','k','l','m',
+						  'n','o','p','q','r','s','t','u','v','w','x','y','z'};
+		String newStr = "";
+		for (int i = 0; i < str.length(); i++){
+			//boolean isLetter = false;
+			for (char l : letters){
+				if (str.charAt(i) == l){
+					newStr += l;
+					break;
+				}
+			}
+
+		}
+		return newStr;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
 		// Replace the following statement with your code
-		return "";
+		char[] letters = new char[str.length()];
+		for (int i = 0; i < str.length(); i++){
+			letters[i] = str.charAt(i);
+		}
+		//System.out.println(letters);
+		for (int i = 0; i < letters.length; i++) {
+			int randomIndex = (int)(Math.random() * letters.length);
+			char temp = letters[i];
+			letters[i] = letters[randomIndex];
+			letters[randomIndex] = temp;
+		}
+		str = String.valueOf(letters);
+		return str;
 	}
 }
